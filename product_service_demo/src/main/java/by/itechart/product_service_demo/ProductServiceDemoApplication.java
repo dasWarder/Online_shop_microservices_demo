d32de3a2b79c474762f915a5f447b34dc4791121
dasWarder <Al.94_94@mail.ru>
2021-07-23 12:39:57 +0300

    Create discover-service with Eurika-server and add Eurika-client to the order and product services

--- product_service_demo/src/main/java/by/itechart/product_service_demo/ProductServiceDemoApplication.java
+++ product_service_demo/src/main/java/by/itechart/product_service_demo/ProductServiceDemoApplication.java
@@ -2,7 +2,9 @@ package by.itechart.product_service_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceDemoApplication {
 
