15a4e52697b2350ff4bf31075c27415a86ff34b1
dasWarder <Al.94_94@mail.ru>
2021-07-26 09:28:12 +0300

    Add gateway service

---
+++ api-gateway-service/src/main/java/by/itechart/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package by.itechart.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+
+}
